d94d2726acd8180108b8d12d35cc879bca7624c2
JorgxVillatoro <villatorojorg@gmail.com>
2026-05-24 15:54:15 -0600

    Corrigiendo estructura de carpetas de Jorge

---
+++ Empleados-jorge/WebForm1.aspx
@@ -0,0 +1,83 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Estudiantes_Jorge.WebForm1" %>
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            width: 505px;
+        }
+        .auto-style3 {
+            width: 42px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <center>    
+                Registro de estudiantes<br />
+            </center>
+        </div>
+        <table class="auto-style1">
+            <tr>
+                <td class="auto-style2">id</td>
+                <td class="auto-style3">:</td>
+                <td>
+                    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                    <asp:Button ID="Button6" runat="server" Height="27px" OnClick="Button6_Click" Text="Search" Width="69px" />
+                    <br />
+                    <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">Nombre</td>
+                <td class="auto-style3">:</td>
+                <td>
+                    <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">Genero</td>
+                <td class="auto-style3">:</td>
+                <td>
+                    <asp:RadioButtonList ID="RadioButtonList1" runat="server">
+                        <asp:ListItem>Masculino</asp:ListItem>
+                        <asp:ListItem>Femenino</asp:ListItem>
+                    </asp:RadioButtonList>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">Email</td>
+                <td class="auto-style3">:</td>
+                <td>
+                    <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">Ciudad</td>
+                <td class="auto-style3">:</td>
+                <td>
+                    <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            </table>
+        <center>   
+        <asp:Button ID="Button1" runat="server" Text="New" OnClick="Button1_Click" />
+        <asp:Button ID="Button2" runat="server" Text="Save" OnClick="Button2_Click" />
+        <asp:Button ID="Button3" runat="server" Text="Update" OnClick="Button3_Click" />
+        <asp:Button ID="Button4" runat="server" Text="Delete" OnClick="Button4_Click" />
+        <asp:Button ID="Button5" runat="server" Text="Show" OnClick="Button5_Click" />
+         </center>
+        <center>    
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+            </center>
+    </form>
+</body>
+</html>
---
+++ Empleados-jorge/WebForm1.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Estudiantes_Jorge
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        OleDbConnection conexion = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\MINEDUCYT\Documents\Proyectos arcides 2026\Estudiantes-Jorge\Empleados-jorge\Estudiantes-Jorge.accdb");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+            TextBox3.Text = "";
+            TextBox4.Text = "";
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            OleDbDataAdapter da = new OleDbDataAdapter("insert into Estudiantes(Nombre,Genero,Email,Ciudad) values('" + TextBox2.Text + "','" + RadioButtonList1.Text + "','" + TextBox3.Text + "','" + TextBox3.Text + "')", conexion);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Datos guardados')", true);
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            OleDbDataAdapter da = new OleDbDataAdapter("update Estudiantes set Nombre='" + TextBox2.Text + "',Genero='" + RadioButtonList1.Text + "',Email='" + TextBox3.Text + "',Ciudad='" + TextBox4.Text + "' where Id=" + TextBox1.Text + "", conexion);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Student updated')", true);
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            OleDbDataAdapter da = new OleDbDataAdapter("delete from Estudiantes where Id=" + TextBox1.Text + "", conexion);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Student delete')", true);
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            OleDbDataAdapter da = new OleDbDataAdapter("select * from Estudiantes", conexion);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            GridView1.DataSource = ds.Tables[0];
+            GridView1.DataBind();
+        }
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button6_Click(object sender, EventArgs e)
+        {
+
+            bool temp = false;
+
+            conexion.Open();
+            OleDbCommand cmd = new OleDbCommand("select * from Estudiantes where Id=" + TextBox1.Text + "", conexion);
+            OleDbDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                TextBox2.Text = dr.GetString(1);
+                RadioButtonList1.Text = dr.GetString(2);
+                TextBox3.Text = dr.GetString(3);
+                TextBox3.Text = dr.GetString(4);
+
+                temp = true;
+            }
+
+            if (temp == false)
+                Label1.Text = "Student not found";
+
+            conexion.Close();
+
+        }
+    }
+}
